d52a91098361092611a145d61fb174a3582a8416
HungChe <hclo@snese.net>
2016-03-30 11:17:52 +0800

    add screenshot plugin function

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,16 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope) {
+  $scope.screenshot = function() {
+    navigator.screenshot.save(function(error, res) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log('screenshot saved in: ', res.filePath);
+      }
+    }, 'jpg', 100, 123);
+  };
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
